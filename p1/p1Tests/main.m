04c67e5dc49a9818b7ca27fcef964489ffc5a26f
Rently Coimbatore <rentlycoimbatore@Rentlys-MacBook-Pro.local>
2024-01-15 16:46:09 +0530

    addinng project file

---
+++ p1/p1Tests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  p1
+//
+//  Created by Rently Coimbatore on 15/01/24.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
---
+++ p1/p1Tests/p1Tests.m
@@ -0,0 +1,36 @@
+//
+//  p1Tests.m
+//  p1Tests
+//
+//  Created by Rently Coimbatore on 15/01/24.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface p1Tests : XCTestCase
+
+@end
+
+@implementation p1Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
